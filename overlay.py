e812511d9a0b697a15e3f90dcc5f1fe62b95ae91
UsmanCyber66 <wes.kaneda@gmail.com>
2026-02-28 12:16:08 +0000

    Wrote code in overlay.py

--- overlay.py
+++ overlay.py
@@ -1,2 +1,36 @@
+import typer
+import subprocess
+
 #!/usr/bin/env python3
 
+app = typer.Typer()
+
+@app.group()
+def create():
+    """Create resources"""
+    pass
+    create_app = typer.Typer()
+
+    @create_app.command()
+    def network():
+        """Create a network"""
+        pass
+
+    @create_app.command()
+    def node():
+        """Create a node"""
+        pass
+
+    app.add_typer(create_app, name="create")
+@create.command()
+def create_network():
+    """Create a network"""
+    pass
+
+@create.command()
+def create_node():
+    """Create a node"""
+    pass
+
+if __name__ == "__main__":
+    app()
